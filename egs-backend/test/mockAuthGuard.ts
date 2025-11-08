ddce5057602c765779300a66793cbd6ce5a46c97
CodingDiederik <djjmwebster@gmail.com>
2025-11-08 20:27:53 +0100

    fix: copilot review

--- egs-backend/test/mockAuthGuard.ts
+++ egs-backend/test/mockAuthGuard.ts
@@ -5,7 +5,7 @@ export class MockAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     // set the user object in the request to simulate an authenticated admin user
     const request = context.switchToHttp().getRequest();
-    const user = { id: 1, email: 'admin@example.com', role: 'admin' };
+    const user = { id: 9999, email: 'admin@example.com', role: 'admin' };
     request.user = user;
     return user && user.role === 'admin';
   }
