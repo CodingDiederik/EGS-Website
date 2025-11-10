3dbcf2895695e9a0cd425d74d2f41f445b02bb40
CodingDiederik <djjmwebster@gmail.com>
2025-11-10 20:22:31 +0100

    fix: rebase errors

--- egs-backend/src/migrations/1762772725675-Migration.ts
+++ egs-backend/src/migrations/1762772725675-Migration.ts
@@ -17,23 +17,3 @@ export class Migration1762772725675 implements MigrationInterface {
         `);
   }
 }
-
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migration1762772725675 implements MigrationInterface {
-  name = 'Migration1762772725675';
-
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            ALTER TABLE "user"
-                RENAME COLUMN "hashedPassword" TO "password"
-        `);
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            ALTER TABLE "user"
-                RENAME COLUMN "password" TO "hashedPassword"
-        `);
-  }
-}
