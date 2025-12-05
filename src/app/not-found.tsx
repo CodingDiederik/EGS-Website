f3bb1f97c285efe3454aa5082d184439f8e0b510
CodingDiederik <djjmwebster@gmail.com>
2025-12-05 20:17:18 +0100

    fix: copilot review

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
-import './not-found.css';
+import styles from './not-found.module.css';
 
 export default function NotFound() {
   return (
-    <div className="not-found">
+    <div className={styles['not-found']}>
       <h1>404 - Pagina niet gevonden &#128577;</h1>
       <p>De opgevraagde pagina bestaat niet.</p>
       <Link href="/">Klik hier om terug naar de homepagina te gaan</Link>
