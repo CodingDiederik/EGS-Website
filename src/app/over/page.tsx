5c5a732f33234c0a210db0e57777c9c150d8c5af
CodingDiederik <107302419+CodingDiederik@users.noreply.github.com>
2026-04-24 15:03:53 +0200

    chore: update trainer text and correclty align image (#116)

--- src/app/over/page.tsx
+++ src/app/over/page.tsx
@@ -85,38 +85,58 @@ export default function Over() {
           imageSrc="/trainer/Jasper.jpeg"
           objectPosition="170% -10px"
           scale={1.6}
-          bio="Jasper is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Binnen Schaakclub EGS is Jasper werkzaam als jeugdleider en trainer voor de hoogste groep van onze club. Hij is heel erg op de hoogte van actualiteiten in de schaakwereld en heeft veel ervaring met het spelen van toernooien en wedstrijden. Verder heeft hij ook al vele jaren theoretische schaakkennis achter de rug en heeft hij veel ervaring binnen EGS. Deze theoretische en praktijkkennis wil hij graag doorgeven aan de volgende generatie schakers.
+Met zijn werk in de zorg, weet hij ook goed hoe hij om moet gaan met niet-schaakgerelateerde problemen en helpt hij graag kinderen.
+Als jeugdleider zorgt hij ervoor dat er op elk niveau passend lesgegeven wordt. Hij neemt daarnaast, samen met Ciske, de organisatie van de jeugdafdeling voor zijn rekening.
+Tot slot is Jasper nauw betrokken bij de kampcommissie en streeft hij ernaar om schaken voor iedereen zo leuk te maken als hij het zelf vindt.
+"
         />
         <TrainerProfile
           name="Ciske"
           imageSrc="/trainer/Ciske.jpeg"
           objectPosition="100% -10px"
-          bio="Ciske is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Ciske is binnen Schaakclub EGS werkzaam als jeugdsecretaris. Hij schaakt al meer dan tien jaar bij onze vereniging en traint zelf om beter te worden. Als jeugdsecretaris is hij verantwoordelijk voor het reilen en zeilen van de jeugd en neemt hij, samen met Jasper, de organisatorische taken van de jeugdafdeling op zich.
+Ciske studeert een bachelor Politicologie aan de Radboud Universiteit te Nijmegen. Hij is nauw betrokken bij politiek en maatschappij en brengt die kennis ook terug in de organisatie van de jeugdschaak.
+De liefde voor organisatie en bestuur neemt hij ook mee naar de kampcommissie, waarbinnen hij actief deelneemt aan de organisatie van het jeugdkamp.
+Hij houdt binnen zijn taken ook contact met de leden en zorgt ervoor dat de club goed en voldoende bereikbaar blijft. Met veel plezier zet hij zich in om de jeugdafdeling van de schaakclub elke dag beter, soepeler en gezelliger te laten werken.
+"
         />
         <TrainerProfile
           name="Sil"
           imageSrc="/trainer/Sil.jpeg"
           scale={1.1}
           objectPosition="5px -20px"
-          bio="Sil is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Sil is in het dagelijks leven aan het studeren om Aardrijkskunde te geven aan middelbare scholieren. Met zijn volle overtuiging en veel plezier geeft hij graag les en leert hij leerlingen nieuwe dingen aan. Niet alleen de cognitieve kennis vergroten is voor Sil belangrijk, maar ook de sociale ontwikkeling van kinderen is voor hem een opdracht. Hij past daarom zeer goed in zijn taak als jeugdtrainer van de stap-1-groep.
+De stap-1-groep is binnen Schaakclub EGS de groep met de jongste kinderen en Sil zet zich ervoor in dat die kinderen niet alleen beter leren schaken, maar ook een grotere sociale ontwikkeling doormaken.
+Dit zie je daarnaast ook terug in Sil zijn actieve bijdrage aan de kampcommissie voor het jaarlijkse jeugdkamp, waar we kinderen uitdagen om zich te laten zien van een andere kant dan ze doen achter het schaakbord. Sil schaakt daarnaast zelf al meer dan tien jaar bij onze vereniging en traint zelf nog om beter te worden.
+"
         />
         <TrainerProfile
           name="Thijs"
           imageSrc="/trainer/Thijs.jpeg"
           objectPosition="5px 0px"
-          bio="Thijs is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Thijs is binnen EGS werkzaam als jeugdtrainer voor de middengroep. Hij zet zich met veel passie en enthousiasme in om leerlingen verder te trainen en hun basisvaardigheden te versterken en uit te bouwen. Daarbij weet hij de spelers te motiveren en uit te dagen om zich stap voor stap te ontwikkelen.
+Thijs is zelf in 2010 als kind begonnen bij EGS en is de club sindsdien altijd trouw gebleven. Door de jaren heen heeft hij niet alleen als speler, maar ook als trainer veel binding met de vereniging opgebouwd.
+Met enige ervaring voor de klas en zijn huidige functie als IT-consultant, brengt hij bovendien waardevolle vaardigheden mee op het gebied van communicatie, samenwerken en het ondersteunen van anderen. In zijn rol als trainer ligt zijn kracht vooral in het creëren van een fijne leeromgeving waarin samenwerking en persoonlijke ontwikkeling centraal staan.
+"
         />
         <TrainerProfile
           name="Niels"
           imageSrc="/trainer/Niels.jpeg"
           objectPosition="10px 0px"
           scale={1.1}
-          bio="Niels is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Niels is binnen de Eerste Goirlese Schaakclub actief als jeugdtrainer van de middengroep en helpt daarnaast regelmatig bij de stap-1-groep. Hij is via de schaakbond opgeleid als trainer en heeft al langere tijd ervaring binnen de club.
+In zijn trainingen staat plezier centraal. Hij vindt het belangrijk dat iedere leerling zich op zijn eigen niveau kan ontwikkelen en hij legt de nadruk op duidelijke uitleg en helpt spelers stap voor stap verder. 
+Niels is de zoon van oud-schaaktrainer Pieter Couwenberg, waardoor hij al op jonge leeftijd met schaken in aanraking kwam. Dat heeft hem gevormd in hoe hij zelf nu training geeft: met aandacht voor zowel het spel als het plezier eromheen.
+Naast het trainen vind je hem ook vaak terug bij de kampcommissie, waar hij graag meedenkt en helpt om activiteiten neer te zetten waar jeugdspelers met plezier aan terugdenken.
+"
         />
         <TrainerProfile
           name="Sjaak"
           imageSrc="/trainer/Sjaak.jpeg"
-          bio="Sjaak is een ervaren schaaktrainer die al vele jaren lesgeeft aan jeugdspelers van alle niveaus. Met zijn geduldige en enthousiaste aanpak weet hij elke leerling te motiveren en te inspireren om het beste uit zichzelf te halen."
+          bio="Binnen Schaakclub EGS speelt Sjaak bij het wedstrijdteam EGS2 en is werkzaam als trainer voor de hoogste groep. Hij wil leerlingen graag voorbereiden en motiveren om mee te doen aan wedstrijden in de schaakwereld van de volwassenen.
+Dankzij zijn werk heeft hij veel ervaring met training, discipline en strategie. Drie essentiële competenties die je nodig hebt om beter te worden in schaken. Hij vindt het belangrijk dat in de hoogste groep niet alleen maar gekeken wordt naar eindeloze schaaktheorie, maar ook naar het plezier in het spel en de sportiviteit onderling.
+"
         />
         <Image
           src="/trainer/Group.jpeg"
